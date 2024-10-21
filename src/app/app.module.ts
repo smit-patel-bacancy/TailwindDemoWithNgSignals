fba331bc39dd2ab8a9f9979c31609f4c139e4cfc
smit-patel-bacancy <smit.patel@bacancy.com>
2024-10-21 18:32:28 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { TodoListSignalsComponent } from './todo-list-signals/todo-list-signals.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    TodoListSignalsComponent
   ],
   imports: [
     BrowserModule,
