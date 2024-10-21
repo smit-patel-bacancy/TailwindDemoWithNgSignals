44cc362468e5b85d77eb7094260b414895b26474
smit-patel-bacancy <smit.patel@bacancy.com>
2024-10-21 18:32:21 +0530

    Update app-routing.module.ts
    routings updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodoListSignalsComponent } from './todo-list-signals/todo-list-signals.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/login' },
+  { path: 'login', component: LoginComponent },
+  { path: 'todo-list', component: TodoListSignalsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
