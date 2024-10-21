47c562a11a6ee387ec293ef6b37e75bddab6add5
smit-patel-bacancy <smit.patel@bacancy.com>
2024-10-21 18:58:35 +0530

    Update notification-service.service.ts
    notification logic and methods added

--- src/services/notification-service.service.ts
+++ src/services/notification-service.service.ts
@@ -5,5 +5,19 @@ import { Injectable } from '@angular/core';
 })
 export class NotificationServiceService {
 
+  public notificationMessage: string = '';
+  public isNotificationShow: boolean = false;
+
   constructor() { }
+
+  public showNotification(notificationText: string): void {
+    this.notificationMessage = notificationText;
+    this.isNotificationShow = true;
+  }
+
+  public closeNotification(): void {
+    this.notificationMessage = '';
+    this.isNotificationShow = false;
+  }
+
 }
