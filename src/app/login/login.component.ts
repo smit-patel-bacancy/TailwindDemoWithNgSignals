725c4a8ff03ceb9d526676e661f591e70ab357bd
smit-patel-bacancy <smit.patel@bacancy.com>
2024-10-21 18:24:25 +0530

    login component added
    - design added
    - formsModule added
    - removed default html code from app.component.html

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  onSubmit() {
+    console.log('Login clicked');
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+  }
+}
